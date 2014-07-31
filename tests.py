77424b02621bee35db43fe64e66e503125d1fb92
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2014-07-31 22:21:09 +0100

    Add tests for conversion of balls string to array

--- tests.py
+++ tests.py
@@ -19,6 +19,26 @@ class SetsBalls(unittest.TestCase):
         self.assertEqual(game.balls, balls)
 
 
+class MakesBallsArray(unittest.TestCase):
+
+    def testBallsArrayLength(self):
+        balls = '45397X93'
+        game = Game(balls)
+        ba = game._make_balls_array()
+        self.assertEqual(len(ba), len(balls))
+
+    def testBallsArrayValues(self):
+        balls = '3200715'
+        game = Game(balls)
+        ba = game._make_balls_array()
+        self.assertEqual(ba, [3,2,0,0,7,1,5])
+
+    def testCatchesBadValues(self):
+        balls = 'O3200715'
+        game = Game(balls)
+        self.assertRaises(ValueError, game._make_balls_array)
+
+
 def main():
     unittest.main()
 
