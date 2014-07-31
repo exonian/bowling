9c1dab71275aff0a412e09e126dc3731ad2c366a
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2014-07-31 22:20:33 +0100

    Fix deprecated way of doing assertions

--- tests.py
+++ tests.py
@@ -2,20 +2,21 @@ import unittest
 
 from score import Game
 
+
 class SetsBalls(unittest.TestCase):
 
     def testNoArg(self):
         game = Game()
-        self.failUnless(game.balls=='')
+        self.assertEqual(game.balls, '')
 
     def testEmptyArg(self):
         game = Game('')
-        self.failUnless(game.balls=='')
+        self.assertEqual(game.balls, '')
 
     def testStringProvided(self):
         balls = '13283332'
         game = Game(balls)
-        self.failUnless(game.balls==balls)
+        self.assertEqual(game.balls, balls)
 
 
 def main():
